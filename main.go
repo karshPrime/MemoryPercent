204f48dd2c4dcfa69ab822b9e5246010cf60722e
karsh <utkarsh@cyberfear.com>
2024-12-09 13:25:31 +1100

    feat: basic structure
    + set up basic project structure

--- main.go
+++ main.go
@@ -2,10 +2,35 @@
 package main
 
 import (
-    //"github.com/karshPrime/MemoryPercentage/cmd"
+    "os"
+    "github.com/karshPrime/MemoryPercentage/cmd"
 )
 
 func main() {
+    lBirthDate := "";
+    lOnDate := "";
+    lTests := [...]bool{ false, false, false, false }
 
+    // fix the check - upgrade with a loop to account for valid
+    // varying sized input args
+
+	if len(os.Args) > 2 {
+        switch os.Args[1] {
+            case "-b":
+                lBirthDate = os.Args[2];
+
+            case "-d": lTests[cmd.Day]   = true;
+            case "-m": lTests[cmd.Month] = true;
+            case "-w": lTests[cmd.Week]  = true;
+            case "-y": lTests[cmd.Year]  = true;
+        }
+	}
+
+    // add logic to call the function with appropriate variables
+
+    cmd.Time_Since( lBirthDate, lOnDate, cmd.Day   );
+    cmd.Time_Since( lBirthDate, lOnDate, cmd.Week  );
+    cmd.Time_Since( lBirthDate, lOnDate, cmd.Month );
+    cmd.Time_Since( lBirthDate, lOnDate, cmd.Year  );
 }
 
